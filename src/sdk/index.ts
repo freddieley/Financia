70a5c6fda4bb77efa950e9a385579b1c83f44101
Freddie Ley <freddie.ley@icloud.com>
2026-08-23 18:09:31 +0100

    feat: export Financia TypeScript SDK

---
+++ src/sdk/index.ts
@@ -0,0 +1,11 @@
+export {
+    FinanciaApiError,
+    FinanciaClient
+} from "./client.ts";
+
+export type {
+    CreateAssetInput,
+    CreateIntentInput,
+    FinanciaClientOptions,
+    IntentExecutionResult
+} from "./client.ts";
